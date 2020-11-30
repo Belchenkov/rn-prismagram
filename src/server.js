4ec83881e6c8b81f4deab66c42797e02c9f62200
Aleksey Belchenkov <u608110@gmail.com>
2020-11-30 17:28:34 +0300

    Creating GraphQL Server

--- src/server.js
+++ src/server.js
@@ -1,1 +1,26 @@
-console.log('Running API');
+import { GraphQLServer } from "graphql-yoga";
+
+require('dotenv').config();
+
+const typeDefs = `
+    type Query {
+        hello: String!
+    }
+`;
+
+const resolvers = {
+    Query: {
+        hello: () => "Hi"
+    }
+};
+
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers
+});
+
+const PORT = process.env.PORT || 4000;
+
+server.start({ port: PORT }, () => {
+    console.log(`Server running on http://localhost:${PORT}`)
+});
